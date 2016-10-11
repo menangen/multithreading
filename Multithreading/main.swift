efe54a7b8ab98d745eaca6860b630708f2770244
menangen <menangen@menangens-imac.local>
2016-10-11 19:25:15 +0300

    Initial Commit

---
+++ Multithreading/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Multithreading
+//
+//  Created by menangen on 11/10/2016.
+//  Copyright © 2016 menangen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
